26a4736b88254cfadb8c02227e70c4bdf363fe13
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-28 22:26:00 -0700

    Added DataService

--- Pods/leveldb-library/db/c.cc
+++ Pods/leveldb-library/db/c.cc
@@ -271,7 +271,7 @@ void leveldb_compact_range(
   Slice a, b;
   db->rep->CompactRange(
       // Pass NULL Slice if corresponding "const char*" is NULL
-      (start_key ? (a = Slice(start_key, start_key_len), &a) : NULL),
+                        (start_key ? (static_cast<void>(a = Slice(start_key, start_key_len)), &a) : NULL),
       (limit_key ? (b = Slice(limit_key, limit_key_len), &b) : NULL));
 }
 
