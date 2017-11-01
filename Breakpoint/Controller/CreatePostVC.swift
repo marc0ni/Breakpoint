7807394ab37d47a8078572ca9a65167bc05e50ce
Mark Lindamood <mark.lindamood@yahoo.com>
2017-11-01 14:54:43 -0700

    Created CreatePostVC view controller and IB, uploaded post to Firebase

---
+++ Breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,50 @@
+//
+//  CreatePostVC.swift
+//  Breakpoint
+//
+//  Created by Mark Lindamood on 10/31/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil  &&  textView.text != "Say something here..."{
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There was an error!")
+                }
+            })
+        }
+        
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
