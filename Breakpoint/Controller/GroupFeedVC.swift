51a8ac2fc7e7e180ade151a47013578f2ccef499
Mark Lindamood <mark.lindamood@yahoo.com>
2017-11-07 14:57:30 -0800

    Initialized Group Data for Group and  Present Group Feed VC

--- Breakpoint/Controller/GroupFeedVC.swift
+++ Breakpoint/Controller/GroupFeedVC.swift
@@ -16,9 +16,23 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var messageTextField: InsetTextField!
     @IBOutlet weak var sendBtn: UIButton!
     
+    var group: Group?
+    
+    func initData(forGroup group: Group) {
+        self.group = group
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sendBtnView.bindToKeyboard()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
     }
 
     @IBAction func backBtnWasPressed(_ sender: Any) {
